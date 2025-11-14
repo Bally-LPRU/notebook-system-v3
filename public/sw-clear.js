808da80facc81d89d8258d9f4987c57d338acad5
Bally <xmasball@g.lpru.ac.th>
2025-11-14 18:14:11 +0700

    fix: force clear cache solution

---
+++ public/sw-clear.js
@@ -0,0 +1,28 @@
+// Force clear all caches
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          console.log('Deleting cache:', cacheName);
+          return caches.delete(cacheName);
+        })
+      );
+    }).then(function() {
+      console.log('All caches cleared!');
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
