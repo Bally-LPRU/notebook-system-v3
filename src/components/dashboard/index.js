0388efdbac98d297d4e5ca6131d75a0fa11011d8
Pisit <xmusball@g.lpru.ac.th>
2026-01-07 12:50:07 +0700

    fix: ลบ QuickActions export จาก dashboard/index.js

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,7 +1,6 @@
 export { default as DashboardStats } from './DashboardStats';
 export { default as RecentActivity } from './RecentActivity';
 export { default as StatsChart } from './StatsChart';
-export { default as QuickActions } from './QuickActions';
 export { default as BorrowingLimitsCard } from './BorrowingLimitsCard';
 export { default as LoanRulesSection } from './LoanRulesSection';
 export { default as CurrentLoansCard } from './CurrentLoansCard';
